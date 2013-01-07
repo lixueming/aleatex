c984fa71c83123c8f36b928e018f5b3ae8d9ad92
lixueming <perfectHM56@gmail.com>
2013-01-07 18:46:43 +0800

    Initial Project

---
+++ app/controllers/library_controller.rb
@@ -0,0 +1,7 @@
+class LibraryController < ApplicationController
+  def index
+  end
+  
+  def new
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AleatexMetro::Application.config.secret_token = 'cd4ac28628d47a32e3c5ce6f927586e1a3420ea415219b8ded5d90bd6c60b79adb5f52fffd4880f617780e7f614474940e45db3de43c6ed05ac9c845621f40b3'
